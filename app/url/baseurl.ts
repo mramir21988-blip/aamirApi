2f22bb69d3110e114cf387e04233858fbd0e4354
Anshu78780 <hunternisha55@gmail.com>
2026-01-22 13:56:08 +0530

    added zteen

--- app/url/baseurl.ts
+++ app/url/baseurl.ts
@@ -2,3 +2,4 @@ export const jabelBaseUrl = 'https://jable.tv/';
 export const hmasterBaseUrl = 'aHR0cHM6Ly94aGFtc3RlcjE5LmNvbS8=';
 export const xozillaBaseUrl = 'https://xozilla.com/';
 export const xp = 'https://xprimehub.my/';
+export const zteen = 'https://www.zteenporn.com/';
